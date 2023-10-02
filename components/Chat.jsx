a7123ff4dd66548a6f476fa51f3e08f4de42d061
Ansal Jain <jainansal@gmail.com>
2023-10-02 21:48:12 +0530

    upd: chat screen

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -1,9 +1,13 @@
 const Chat = () => {
   return (
-    <div className="bg-slate-900 p-4 h-3/4 w-1/2 rounded-lg">
+    <div className="bg-slate-900 p-4 h-3/4 w-3/4 lg:w-1/2 rounded-lg flex flex-col gap-2">
+      {/* Conversation */}
+      <div className="h-full rounded echo-overlay"></div>
 
+      {/* User Input */}
+      <div className="h-16 bg-slate-800 rounded"></div>
     </div>
-  )
+  );
 };
 
 export default Chat;
