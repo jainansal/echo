eca756c6d077e407eaa377a8f77b1b366398d564
Ansal Jain <jainansal@gmail.com>
2023-10-03 23:31:21 +0530

    upd: chat screen

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -1,11 +1,16 @@
+import Message from "./Message";
+
 const Chat = () => {
   return (
-    <div className="bg-slate-900 p-4 h-3/4 w-3/4 lg:w-1/2 rounded-lg flex flex-col gap-2">
+    <div className="bg-slate-900 p-3 h-3/4 w-3/4 lg:w-1/2 rounded-lg flex flex-col gap-2">
       {/* Conversation */}
-      <div className="h-full rounded echo-overlay"></div>
+      <div className="h-full rounded echo-overlay flex flex-col gap-2">
+        <Message />
+        <Message />
+      </div>
 
       {/* User Input */}
-      <div className="bg-slate-800 rounded p-4 flex items-center">
+      <div className="bg-slate-800 rounded p-3 flex items-center">
         <input
           type="text"
           placeholder="New message..."
---
+++ components/Message.jsx
@@ -0,0 +1,10 @@
+const Message = () => {
+  return (
+    <div className="rounded p-2 bg-slate-800">
+      <p className="text-xs font-thin text-slate-500">User221</p>
+      <p>Hi there</p>
+    </div>
+  );
+};
+
+export default Message;
