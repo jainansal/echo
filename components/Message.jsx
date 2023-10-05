830074ba53b0815722cbf555e98063f916168c4b
Ansal Jain <jainansal@gmail.com>
2023-10-06 01:06:51 +0530

    upd(css): word wrap

--- components/Message.jsx
+++ components/Message.jsx
@@ -1,16 +1,18 @@
 const Message = ({ message, sender, clientId }) => {
   if (!sender) {
-    return <div className="self-center">{message}</div>;
+    return <div className="self-center break-words">{message}</div>;
   }
 
   return (
     <div
-      className={`rounded p-2 bg-slate-800 ${
+      className={`rounded p-2 bg-slate-800 w-3/4 ${
         sender.id === clientId ? "self-end" : "self-start"
       }`}
     >
-      <p className="text-xs font-thin text-slate-500">{sender.username}</p>
-      <p>{message}</p>
+      <p className="text-xs font-thin text-slate-500 cursor-pointer">
+        {sender.username}
+      </p>
+      <p className="break-words">{message}</p>
     </div>
   );
 };
