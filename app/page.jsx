cbc5b1a9b542aa95adc57573f7de4e163a2e5d1c
Ansal Jain <jainansal@gmail.com>
2023-10-05 23:18:55 +0530

    upd: minor

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,6 @@ import Chat from "@/components/Chat";
 const Home = () => {
   return (
     <div className="echo-view flex items-center justify-center flex-col gap-2">
-      <h1>Welcome, user332</h1>
       <Chat />
     </div>
   );
