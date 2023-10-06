86e0f9121e76b5a56392e8ac60cad7a9c3fd0ce9
Ansal Jain <jainansal@gmail.com>
2023-10-06 23:02:37 +0530

    ft: add react-feather, message options

--- components/Message.jsx
+++ components/Message.jsx
@@ -1,7 +1,22 @@
+"use client";
+
+import { useState } from "react";
+
+import { ChevronDown } from "react-feather";
+
 const Message = ({ message, sender, clientId }) => {
   if (!sender) {
     return <div className="self-center break-words">{message}</div>;
   }
+
+  // useState
+  const [showOptions, setShowOptions] = useState(false);
+
+  // Functions
+  const handleHover = () => {
+    setShowOptions(true);
+    setTimeout(() => setShowOptions(false), 2000);
+  };
 
   return (
     <div
@@ -9,9 +24,23 @@ const Message = ({ message, sender, clientId }) => {
         sender.id === clientId ? "self-end" : "self-start"
       }`}
     >
-      <p className="text-xs font-thin text-slate-500 cursor-pointer">
-        {sender.username}
-      </p>
+      <div className="flex items-center justify-between relative">
+        <p className="text-xs font-thin text-slate-500 cursor-pointer">
+          {sender.username}
+        </p>
+        <ChevronDown
+          size={16}
+          className="cursor-pointer"
+          onMouseEnter={handleHover}
+        />
+        {showOptions && (
+          <div className="absolute top-3 right-0 bg-slate-950 rounded-lg p-2">
+            <p className="text-sm font-thin text-slate-500 cursor-pointer hover:text-slate-300">
+              Reply
+            </p>
+          </div>
+        )}
+      </div>
       <p className="break-words">{message}</p>
     </div>
   );
