0f226e9af498129ba85a4e16542d5af049433b07
Ansal Jain <jainansal@gmail.com>
2023-10-09 02:23:16 +0530

    upd: code clean

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,15 +8,6 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html>
-      {/* <head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-          integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
-          crossorigin="anonymous"
-          referrerpolicy="no-referrer"
-        />
-      </head> */}
       <body className="bg-slate-950 text-slate-200">{children}</body>
     </html>
   );
